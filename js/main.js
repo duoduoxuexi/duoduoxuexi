e25d33dc3acac9ce3a44eec2814b09d65e7a571f
duoduoxuexi <110448033+duoduoxuexi@users.noreply.github.com>
2022-11-30 20:30:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -285,7 +285,7 @@ document.oncontextmenu = function () {
 var myDate = new Date;
 var mon = myDate.getMonth() + 1;
 var date = myDate.getDate();
-var days = ['4.4', '5.12', '7.7', '9.9', '9.18', '12.13'];
+var days = ['4.4', '5.12', '7.7', '9.9', '9.18','11.30', '12.13'];
 for (var day of days) {
     var d = day.split('.');
     if (mon == d[0] && date == d[1]) {
